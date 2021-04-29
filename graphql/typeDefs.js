632c95a5cf876305e80b5ea3dfe99d83776db333
Yasha <ysahakyan1998@gmail.com>
2021-04-29 13:26:48 +0400

    Start

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,25 +1,23 @@
-const {gql} = require("apollo-server");
+const { gql } = require("apollo-server");
 
 module.exports = gql`
-    type User {
-        firstname: String!
-        lastname: String!
-        email: String!
-        createdAt: String!
-    }
-    
-    type Query {
-        users:[User]
-        login(email:String!,passwoed:String):User!
-    }
-    
-    type Mutation {
-        register(
-            firstname: String!
-            lastname: String!
-            password: String!
-            email: String!
-        ): User!
+  type User {
+    firstname: String!
+    lastname: String!
+    email: String!
+  }
 
-    }
+  type Query {
+    users: [User]!
+    login(email: String!, password: String!): User!
+  }
+
+  type Mutation {
+    register(
+      firstname: String!
+      lastname: String!
+      password: String!
+      email: String!
+    ): User!
+  }
 `;
